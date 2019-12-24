a50b9dc05cc769da7c09ea3fb2dfdca92ed16405
Mahdi Bagheri <Bagheri@office.dml.com>
2019-12-24 16:04:40 +0330

    implement service

---
+++ Hooshyar/Hooshyar.Service/EfService/ItemService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hooshyar.Data;
+using Hooshyar.Model;
+using Hooshyar.Service.Interface;
+
+namespace Hooshyar.Service.EfService
+{
+	public class ItemService: IItemService
+	{
+		private IUnitOfWork _unitOfWork;
+		private DbSet<Item> _items;
+
+		public ItemService(IUnitOfWork unitOfWork)
+		{
+			_unitOfWork = unitOfWork;
+			_items = _unitOfWork.Set<Item>();
+		}
+		public void AddItem(Item item)
+		{
+			_items.Add(item);
+		}
+
+		public Item Find(int id)
+		{
+			return _items.Find(id);
+			
+		}
+
+		public void Update(Item item)
+		{
+			var dbItem = _items.Single(a => a.Id == item.Id);
+			dbItem.Name = item.Name;
+			dbItem.CountType = item.CountType;
+			dbItem.Count = item.Count;
+		}
+
+		public void Delete(Item item)
+		{
+			_items.Remove(item);
+		}
+
+		public IList<Item> GetAll()
+		{
+			return _items.ToList();
+		}
+	}
+}
---
+++ Hooshyar/Hooshyar.Service/Interface/IItemService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Hooshyar.Model;
+
+namespace Hooshyar.Service.Interface
+{
+	public interface IItemService
+	{
+		void AddItem(Item item);
+		Item Find(int id);
+		void Update(Item item);
+		void Delete(Item item);
+		IList<Item> GetAll();
+	}
+}
