d4b2318182be04c0f6c7b4a51a4be3654f00a3b5
Mahdi Bagheri <Bagheri@office.dml.com>
2019-12-24 16:50:06 +0330

    complete iocconfig

---
+++ Hooshyar/Hooshyar.IocConfig/SmObjectFactory.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Hooshyar.Data;
+using Hooshyar.Service.EfService;
+using Hooshyar.Service.Interface;
+using StructureMap;
+using StructureMap.Graph;
+using StructureMap.Web;
+
+namespace Hooshyar.IocConfig
+{
+	public static class SmObjectFactory
+	{
+		private static readonly Lazy<Container> _containerBuilder =
+			new Lazy<Container>(defaultContainer, LazyThreadSafetyMode.ExecutionAndPublication);
+
+		public static IContainer Container
+		{
+			get { return _containerBuilder.Value; }
+		}
+
+		private static Container defaultContainer()
+		{
+			var container = new Container(ioc =>
+			{
+				
+				ioc.Scan(scan =>
+				{
+					scan.TheCallingAssembly();
+					scan.WithDefaultConventions();
+				});
+				ioc.For<IUnitOfWork>().HybridHttpOrThreadLocalScoped().Use<HooshyarDbContext>();
+				ioc.For<HooshyarDbContext>().HybridHttpOrThreadLocalScoped().Use(context => (HooshyarDbContext)context.GetInstance<IUnitOfWork>());
+				ioc.For<DbContext>().HybridHttpOrThreadLocalScoped().Use(context => (HooshyarDbContext)context.GetInstance<IUnitOfWork>());
+
+				ioc.For<IItemService>().Use<ItemService>();
+
+				//ioc.Policies.SetAllProperties(y =>
+				//{
+				//	y.OfType<IUnitOfWork>();
+				//	y.OfType<IItemService>();
+				//	y.OfType<DbContext>();
+				//});
+
+			});
+			return container;
+		}
+	}
+}
--- Hooshyar/Hooshyar.WebApi/App_Start/WebApiConfig.cs
+++ Hooshyar/Hooshyar.WebApi/App_Start/WebApiConfig.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Formatting;
 using System.Web.Http;
+using System.Web.Http.Dispatcher;
+using Hooshyar.IocConfig;
+using Newtonsoft.Json;
 
 namespace Hooshyar.WebApi
 {
@@ -9,16 +13,24 @@ namespace Hooshyar.WebApi
     {
         public static void Register(HttpConfiguration config)
         {
-            // Web API configuration and services
+	        var container = SmObjectFactory.Container;
+	        GlobalConfiguration.Configuration.Services.Replace(
+		        typeof(IHttpControllerActivator), new StructureMapHttpControllerActivator(container));
 
-            // Web API routes
-            config.MapHttpAttributeRoutes();
+			// Web API configuration and services
+			// Web API routes
+			config.MapHttpAttributeRoutes();
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
-        }
+
+	        //config.Formatters.Clear();
+	        //config.Formatters.Add(new JsonMediaTypeFormatter());
+	        //config.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+	        //config.Formatters.JsonFormatter.Indent = true;
+		}
     }
 }
